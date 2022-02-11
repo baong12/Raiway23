e31a9b132aec737608ca47568a8d2810f27ef641
Bao Nguyen <bao.nguyenkim12@gmail.com>
2022-02-12 00:20:20 +0700

    CRUD update

--- FrontendBasic/CRUD/js/group_controller.js
+++ FrontendBasic/CRUD/js/group_controller.js
@@ -1,12 +1,15 @@
 var dataTable = document.getElementById("data-table");
+var dataTableBody = document.getElementById("data-table-body");
 var dataTableHeader = dataTable.innerHTML;
+
+/* Bindings ---------------- */
 
 $(document).ready(function () {
     getGroups(renderTable);
 });
 
 $("#add").click(function () {
-    loadDialog("/dialogs/group_add.html");
+    loadDialog("dialogs/group_add.html");
     open_modal();
 });
 
@@ -17,6 +20,16 @@ $("#refresh").click(function () {
 $("#delete").click(function () {
     deleteData();
 });
+
+$("#searchBox").keydown(function (event) {
+    let keyCode = event.which;
+    if (keyCode == 13) {
+        let word = $("#searchBox").val();
+        searchData("groupName", word);
+    }
+});
+
+/* Functions --------------- */
 
 function selectAll() {
     let state = document.getElementById("select-all").checked;
@@ -29,11 +42,8 @@ function selectAll() {
 var curRow;
 function openEditGroupDlg(button) {
     curRow = $(button).parents("tr");
-    loadDialog("/dialogs/group_edit.html");
+    loadDialog("dialogs/group_edit.html");
     open_modal();
-    // $(document).ready(function () {
-        // loadCurGroupData();
-    // });
 }
 
 function loadCurGroupData() {
@@ -41,23 +51,24 @@ function loadCurGroupData() {
     $("#grName").val($(curRow).children(".groupName").text());
     $("#member").val($(curRow).children(".member").text());
     $("#creator").val($(curRow).children(".creator").text());
-    $("#createDate").val($(curRow).children(".createDate").text().replace("Z",""));
+    $("#createDate").val($(curRow).children(".createDate").text().replace("Z", ""));
 }
 
 function renderTable(data) {
+    dataTable.innerHTML = "";
     let content = dataTableHeader;
     data.forEach((element) => {
-        content += "<tr>" +
-            "<td><input type='checkbox' name='select'></td>" +
-            "<td class='id'>" + element.id + "</td>" +
-            "<td class='groupName'>" + element.groupName + "</td>" +
-            "<td class='member'>" + element.member + "</td>" +
-            "<td class='creator'>" + element.creator + "</td>" +
-            "<td class='createDate'>" + element.createDate + "</td>" +
-            "<td><button onclick='openEditGroupDlg(this)'>" +
-                "<i class='fa fa-edit fa-fw'></i>" +
-            "</button></td>" +
-            "</tr>";
+        content += `<tr>
+                <td><input type='checkbox' name='select'></td>
+                <td class='id'>${element.id}</td>
+                <td class='groupName'>${element.groupName}</td>
+                <td class='member'>${element.member}</td>
+                <td class='creator'>${element.creator}</td>
+                <td class='createDate'>${element.createDate.slice(0, 16)}</td>
+                <td>
+                    <button onclick='openEditGroupDlg(this)'><i class='fa fa-pen fa-fw'></i></button>
+                </td>
+            </tr>`;
     });
     dataTable.innerHTML = content;
 }
@@ -71,30 +82,40 @@ function postData() {
     postGroup(data);
     getGroups(renderTable);
     close_modal();
+    alertify.success(`Tạo thành công`);
 }
 
 function deleteData() {
     let cells = $("tr > td > :checked");
-    let idList = [];
+    let deleteList = [];
     $(cells).each(function () {
-        let id = $(this).parent().siblings(".id").text();
-        idList.push(id);
+        let d_id = $(this).parent().siblings(".id").text();
+        let d_groupName = $(this).parent().siblings(".groupName").text();
+        deleteList.push({
+            id: d_id,
+            groupName: d_groupName
+        });
     });
-    idList.forEach(groupId => {
-        deleteGroup(groupId);
+    deleteList.forEach(group => {
+        deleteGroup(group.id);
+        alertify.success(`Xóa thành công: ${group.groupName}`);
     });
     getGroups(renderTable);
 }
 
 function updateData() {
-    alert($("#createDate").val());
-    // updateGroup({
-    //     id: $("#curId").text(),
-    //     name: $("#grName").val(),
-    //     member: $("#member").val(),
-    //     creator: $("#creator").val(),
-    //     createDate: $("#createDate").val()
-    // });
-    // getGroups(renderTable);
+    updateGroup({
+        id: $("#curId").text(),
+        name: $("#grName").val(),
+        member: $("#member").val(),
+        creator: $("#creator").val(),
+        createDate: $("#createDate").val()
+    });
+    getGroups(renderTable);
     close_modal();
+    alertify.success(`Cập nhật thành công`);
+}
+
+function searchData(field, word) {
+    searchGroup(field, word, renderTable);
 }
--- FrontendBasic/CRUD/js/group_service.js
+++ FrontendBasic/CRUD/js/group_service.js
@@ -3,6 +3,7 @@ var DATA_URL = "https://61ed284cf3011500174d230b.mockapi.io/api/v1/groups";
 
 function getGroups(renderer) {
     $.ajax({
+        async: false,
         url: DATA_URL,
         type: "GET",
         success: (result) => renderer(result)
@@ -11,6 +12,7 @@ function getGroups(renderer) {
 
 function postGroup(group) {
     $.ajax({
+        async: false,
         url: DATA_URL,
         type: "POST",
         data: {
@@ -23,20 +25,31 @@ function postGroup(group) {
 
 function updateGroup(group) {
     $.ajax({
+        async: false,
         url: DATA_URL + "/" + group.id,
         type: "PUT",
         data: {
             groupName: group.name,
             member: group.member,
-            creator: group.creator,
-            createDate: group.createDate
-        },
+            // creator: group.creator,
+            // createDate: group.createDate
+        }
     });
 }
 
 function deleteGroup(id) {
     $.ajax({
+        async: false,
         url: DATA_URL + "/" + id,
         type: "DELETE"
+    });
+}
+
+function searchGroup(field, word, renderer) {
+    $.ajax({
+        async: false,
+        url: DATA_URL + "?" + field + "=" + word,
+        type: "GET",
+        success: (result) => renderer(result)
     });
 }
